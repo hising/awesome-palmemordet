d2de01297126ed0cf1a52f518cedd4a21c797975
Mattias Hising <hising@gmail.com>
2019-04-03 20:36:38 +0200

    Typescript on Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 module.exports = {
+    entry: "./src/index.ts",
     module: {
         rules: [
             {
-                test: /\.js/,
+                test: /\.([jt])s/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader?optional=runtime&cacheDirectory=true',
+                    loader: "babel-loader?optional=runtime&cacheDirectory=true",
                     options: {
-                        presets: ['@babel/preset-env'],
+                        presets: ["@babel/preset-env", "@babel/typescript"],
+                        plugins: [
+                            "@babel/proposal-class-properties",
+                            "@babel/proposal-object-rest-spread"
+                        ]
                     }
                 }
             }
