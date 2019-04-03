489ce9100cca1f139aec3839d30983a5ee327ef8
Mattias Hising <hising@gmail.com>
2019-04-03 18:28:16 +0200

    Added some basics

--- event.ts
+++ event.ts
@@ -1,16 +1,16 @@
 export type EventType = "event" | "sighting";
 
 export interface IMOPEvent {
-        type: EventType;
-        label: string;
-        address: string;
-        latitude: number;
-        longitude: number;
-        time: string;
-        accuracy: number;
-        people: string[];
-        reliability: number;
-        places: string[];
-        id: string;
-        sources: string[];
-    }
+    type: EventType;
+    label: string;
+    address: string;
+    latitude: number;
+    longitude: number;
+    time: string;
+    accuracy: number;
+    people: string[];
+    reliability: number;
+    places: string[];
+    id: string;
+    sources: string[];
+}
